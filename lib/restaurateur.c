f476435e61ff152315169d7f752ce5ac41c3b7b6
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 15:41:19 +0200

    l'ajout d'une fonction dans restaurateur.c
    l'ajout de la fonction ajoutRestaurateurConstructeur() dans restaurateur.c

--- lib/restaurateur.c
+++ lib/restaurateur.c
@@ -4,3 +4,20 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+
+prestaurateur ajoutRestaurateurConstructeur(char * id , char * nom , char* codePostal , char * telephone , char * type , char * menu , char * solde){
+    
+    prestaurateur nouveaurestaurateur ;
+    nouveaurestaurateur = malloc(sizeof(char*)*8);
+    nouveaurestaurateur->id=id;
+    nouveaurestaurateur->telephone=telephone;
+    nouveaurestaurateur->solde=solde;
+    nouveaurestaurateur->menu=menu;
+    nouveaurestaurateur->type=type;
+    nouveaurestaurateur->codePostal=codePostal;
+    nouveaurestaurateur->nom=nom;
+    return nouveaurestaurateur;
+    
+
+}
