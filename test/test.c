5304b83000077f91c7cff097cdbda37247722b15
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 16:29:11 +0200

    completer des test pour restaurateur et livreur

--- test/test.c
+++ test/test.c
@@ -59,18 +59,18 @@ int main()
     }
         
         
-                             // test pour des fonctions dans livreur.c ( ajoutLIvreur() , ajoutLivreurConstructeur() , chercherLigneIdLivreur() )
+                             // test pour des fonctions dans Restaurateur.c ( ajoutRestaurateur() , ajoutrestaurateurConstructeur() , chercherLigneIdRestaurateur() )
    {   
 
 
 
         int err2   , err1 ;
-        char * ligne1 = chercherLigneIdRestaurateur( 2 , "dataBase/tableRestaurants.csv" , &err2);        
-        TEST(strcmp(ligne1,  "2,Le Veg,13005,04 10 11 12 13,Vegetarien,2;3;4,24\n" )==0);
+        char * ligne1 = chercherLigneIdRestaurateur( 2 , "dataBase/tableRestaurants.csv" , &err2);  
+        TEST(strcmp(ligne1,  "2,Le Veg,13005,04 10 11 12 13,Vegetarien,2;3;4,24\n" )==0);// voir si on a bien recupérer la ligne qu'on veut       
         char * ligne2 = chercherLigneIdRestaurateur( 1 , "dataBase/tableRestaurants.csv" , &err1);        
-        TEST(strcmp(ligne2,  "1,Chez Michel,13001,04 13 13 13 13,Provencal,1;4;5,50\n" )==0);
-        TEST( err1 == 0 );
-        TEST( err2 == 0);
+        TEST(strcmp(ligne2,  "1,Chez Michel,13001,04 13 13 13 13,Provencal,1;4;5,50\n" )==0);// voir si on a bien recupérer la ligne qu'on veut 
+        TEST( err1 == 0 ); // voir si le fichier s'est ouvert correctement
+        TEST( err2 == 0); // voir si le fichier s'est ouvert correctement
         
         prestaurateur RestaurateurTest = (prestaurateur)malloc(sizeof(restaurateur));
         RestaurateurTest = ajoutRestaurateurConstructeur( "3", "Joe's International House of Pancakes" , "13010" , "04 22 33 44 55" , "Americain" , "6;7" , "44" );
@@ -83,21 +83,57 @@ int main()
         TEST( 0==strcmp(RestaurateurTest->menu , "6;7" ));
         TEST( 0==strcmp(RestaurateurTest->solde , "44" ));
 
-        err1=3;
+        err1=3;// changer la valeur pour les  prochaines tests
 
         ajoutRestaurateur(RestaurateurTest , &err1 , "fichierQuiNexistePas.csv");
-        TEST( err1 == 1 );        
-        err1=3;
+        TEST( err1 == 1 );  // voir si le fichier s'est ouvert correctement       
+        err1=3;// changer la valeur pour les  prochaines tests
         ajoutRestaurateur(RestaurateurTest , &err1 , "dataBase/testDataBase/emptyFileRestaurateur.csv");
-        TEST_FILE( "dataBase/testDataBase/emptyFileRestaurateur.csv" , "dataBase/testDataBase/testEmptyFileRestaurateur.csv"  );
-        TEST(err1 ==0 ) ;
+        TEST_FILE( "dataBase/testDataBase/emptyFileRestaurateur.csv" , "dataBase/testDataBase/testEmptyFileRestaurateur.csv"  );// voir si on a bien écrit la chose qu'on a voulu 
+        TEST(err1 ==0 ) ; // voir si le fichier s'est ouvert correctement
 
 
 
 
 
     }
+        
+         // test pour des fonctions dans livreur.c ( ajoutLIvreur() , ajoutLivreurConstructeur() , chercherLigneIdLivreur() )
+    {
+
+        int err2   , err1 ;
+        char * ligne1 = chercherLigneIdLivreur(1 , "dataBase/tableLivreurs.csv" , &err2);        
+        TEST(strcmp(ligne1,  "1,Francois Pignon,06 00 00 00 00,13001;13002;13003,1,20\n" )==0);// voir si on a bien recupérer la ligne qu'on veut       
+        char * ligne2 = chercherLigneIdLivreur(3 , "dataBase/tableLivreurs.csv" , &err1);        
+        TEST(strcmp(ligne2,  "3,Mickey Mouse,06 11 22 33 44,13008;13009;13010;13011,0,0" )==0);// voir si on a bien recupérer la ligne qu'on veut       
+        TEST( err1 == 0 ); // voir si le fichier s'est ouvert correctement
+        TEST( err2 == 0); // voir si le fichier s'est ouvert correctement
+
+        err1 = 3 ;// changer la valeur pour les  prochaines tests
+        plivreur livreurTest = (plivreur)malloc(sizeof(livreur));
+        livreurTest = ajoutLivreurConstructeur( "2", "Donald Duck" , "06 01 02 03 04" ,"13001;13004;13005;13006;13009;13010", "0" , "25" );
   
+        TEST(0== strcmp(livreurTest->id , "2") );
+        TEST( 0==strcmp(livreurTest->nom , "Donald Duck"));
+        TEST( 0==strcmp(livreurTest->telephone , "06 01 02 03 04") );
+        TEST( 0==strcmp(livreurTest->solde , "25" ));
+        TEST( 0==strcmp(livreurTest->deplacements , "13001;13004;13005;13006;13009;13010") );
+        TEST( 0 == strcmp(livreurTest->restaurant , "0" ) );
+        ajoutLivreur(livreurTest , &err1 , "fichierQuiNexistePas.csv");
+        TEST( err1 == 1 ); // voir si le fichier s'est ouvert correctement
+
+        err1 = 3 ; // changer la valeur pour les  prochaines tests
+        ajoutLivreur(livreurTest , &err1 , "dataBase/testDataBase/emptyFileLivreur.csv");
+        TEST_FILE( "dataBase/testDataBase/testEmptyFileLivreur.csv" , "dataBase/testDataBase/testEmptyFileLivreur.csv"  );// voir si on a bien écrit la chose qu'on a voulu 
+        TEST(err1 ==0 ) ; // voir si le fichier s'est ouvert correctement
+        TEST(err1 ==0 ) ; // voir si le fichier s'est ouvert correctement
+
+
+
+    }
+  
+        
+        
 
     return tests_executed - tests_successful;
 }
