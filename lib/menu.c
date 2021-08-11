10da8b84d23e2198d4482d7cd396ae1eb46ecf62
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 16:41:12 +0200

    ajout de la fonction ajoutMenu() dans menu.c

--- lib/menu.c
+++ lib/menu.c
@@ -18,3 +18,29 @@ pmenu ajoutMenuConstructeur(char * id , char * nom , char* ingredients , char * 
 
 }
 
+
+void  ajoutMenu(pmenu menu , int * err , char * path  ){
+    
+    FILE * tablemenu ;
+
+
+c
+    if( access( path, F_OK ) == 0 ) {// regarder si le fichier exist avec la fonction access( ) de <unistd.h> car le parametre a ou a+ crée toujours le fichier s'il n'existe pas c
+        *err = 0;//s'il n'y a pas une erreur d'ouverture alors err va prendre la valeur 0
+        tablemenu = fopen(path, "a+" );
+        fprintf(tablemenu, "%s,%s,%s,%s%s", menu->id, menu->nom, menu->ingredients, menu->prix, "\n");
+        fclose(tablemenu);
+        return ;  
+
+    }else {
+
+
+        *err = 1; // s'il y a une erreur d'ouverture alors err va prendre la valeur 1
+        printf("fichier non trouvé dans 'ajoutmenu' function \n");
+        return;
+
+
+    }
+
+
+}
