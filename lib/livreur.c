e549da596f6d1c4761892502cca2c6b046155e73
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 15:05:43 +0200

    ajout d'une fonction ajoutLivreur()

--- lib/livreur.c
+++ lib/livreur.c
@@ -22,3 +22,38 @@ plivreur ajoutLivreurConstructeur(char * id , char * nom , char* telephone , cha
     
 
 }
+
+
+void  ajoutLivreur(plivreur livreur , int * err , char * path  ){
+    
+    FILE * tablelivreur ;
+
+
+
+    if( access( path, F_OK ) == 0 ) { // regarder si le fichier exist avec la fonction access de <unistd.h> car le parametre a ou a+ crée toujours le fichier s'il n'existe pas 
+        
+        *err = 0;// s'il n'y a pas une erreur d'ouverture alors err va prendre la valeur 0
+        tablelivreur = fopen(path, "a+" );
+        fprintf(tablelivreur, "%s,%s,%s,%s,%s,%s%s", livreur->id, livreur->nom ,livreur->telephone,  livreur->deplacements, livreur->restaurant , livreur->solde,  "\n");
+        fclose(tablelivreur);
+        return ;  
+
+    }else {
+
+
+        *err = 1;// s'il y a une erreur d'ouverture alors err va prendre la valeur 1
+        printf("fichier non trouvé dans 'ajoutlivreur' function \n");
+        return;
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
