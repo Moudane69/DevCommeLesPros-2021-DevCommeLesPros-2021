3d6b0730b381605df721a7288dfa454c0627b217
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 14:17:14 +0200

    ajout de la fonction ajoutClient()
    la fonction est adapté au nombre des collones du la table client

--- lib/client.c
+++ lib/client.c
@@ -21,3 +21,31 @@ pclient ajoutClientConstructeur(char * id , char * codePostal , char* telephone 
     
 
 }
+
+
+
+void  ajoutClient(pclient client , int * err , char * path  ){
+    
+    FILE * tableClient ;
+
+
+
+    if( access( path, F_OK ) == 0 ) { // regarder si le fichier exist avec la fonction access de <unistd.h> car le parametre a ou a+ crée toujours le fichier s'il n'existe pas 
+        *err = 0; // s'il n'y a pas une erreur d'ouverture alors err va prendre la valeur 1
+        tableClient = fopen(path, "a+" );
+        fprintf(tableClient, "%s%s%s%s%s%s%s%s%s%s", client->id, "," , client->nom, "," ,client->codePostal, "," , client->telephone,"," , client->solde , "\n");
+        fclose(tableClient);
+        return ;  
+
+    }else {
+
+
+        *err = 1; // s'il y a une erreur d'ouverture du fichier alors err va prendre la valeur 1
+        printf("fichier non trouvé dans 'ajoutClient' function \n");
+        return;
+
+
+    }
+
+
+}
