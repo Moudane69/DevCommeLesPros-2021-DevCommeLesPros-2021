626515eda89ace00ab7aca8c458d5e695367070e
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 14:09:19 +0200

    modification client.h
    l'ajout des prototypes de quelques fonctions dans le fichier client.h

---
+++ lib/client.h
@@ -0,0 +1,33 @@
+#ifndef CLIENT
+
+#define CLIENT
+
+
+
+
+typedef struct client{
+
+    char* id;
+    char * nom;
+    char * codePostal; 
+    char *  telephone; 
+    char* solde;
+   
+
+
+
+}client , * pclient;
+
+ // pour ajouter un client on procéde comme ca ajoutrestaurateur( ajoutrestaurateurConstructeur(id , ...) , ...)
+
+
+void ajoutClient(pclient client , int * err, char * path );
+pclient ajoutClientConstructeur(char * id , char * codePostal , char* telephone , char * solde ,char * nom);
+char * chercherLigneIdClient(int i, char * path , int* err);
+
+
+
+
+
+
+#endif
