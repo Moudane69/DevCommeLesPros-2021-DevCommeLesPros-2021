b017ac8c57e72cbd4daf2eed4a981b09fa5e7a94
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 15:44:50 +0200

    ajout d'une fonction  dans restaurateur.c
    l'ajout d'une fonction:  ajoutRestaurateur() dans restaurateur.c

--- lib/restaurateur.c
+++ lib/restaurateur.c
@@ -21,3 +21,30 @@ prestaurateur ajoutRestaurateurConstructeur(char * id , char * nom , char* codeP
     
 
 }
+
+
+void  ajoutRestaurateur(prestaurateur restaurateur , int * err , char * path  ){
+    
+    FILE * tablerestaurateur ;
+
+
+    // regarder si le fichier exist avec la fonction access( ) de <unistd.h> car le parametre a ou a+ crée toujours le fichier s'il n'existe pas 
+    if( access( path, F_OK ) == 0 ) { 
+        *err = 0; // s'il n'y a pas une erreur d'ouverture alors err va prendre la valeur 0
+        tablerestaurateur = fopen(path, "a+" );
+        fprintf(tablerestaurateur, "%s,%s,%s,%s,%s,%s,%s%s", restaurateur->id,  restaurateur->nom, restaurateur->codePostal,restaurateur->telephone,  restaurateur->type, restaurateur->menu, restaurateur->solde , "\n");
+        fclose(tablerestaurateur);
+        return ;  
+
+    }else {
+
+
+        *err = 1; // s'il y a une erreur d'ouverture alors err va prendre la valeur 1
+        printf("fichier non trouvé dans 'ajoutrestaurateur' function \n");
+        return;
+
+
+    }
+
+
+}
