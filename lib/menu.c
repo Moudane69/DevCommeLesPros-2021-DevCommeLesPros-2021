3ed543cb627327449bf59991574741b139e09956
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 16:39:03 +0200

    ajout de la fonction ajoutMenuConstructeur()

--- lib/menu.c
+++ lib/menu.c
@@ -5,5 +5,16 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+ 
+pmenu ajoutMenuConstructeur(char * id , char * nom , char* ingredients , char * prix ){
+    pmenu nouveaumenu ;
+    nouveaumenu = malloc(sizeof(char*)*5);
+    nouveaumenu->id=id;
+    nouveaumenu->ingredients=ingredients;
+    nouveaumenu->prix=prix;
+    nouveaumenu->nom=nom;
+    return nouveaumenu;
+    
 
+}
 
