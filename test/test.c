895bac95cacf0066cbc152fd1d7eeea9e02a0877
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-11 15:51:54 +0200

    ajout de quelques test pour restaurateur.c
    les tests passent avec succes

--- test/test.c
+++ test/test.c
@@ -96,6 +96,46 @@ int main()
 
 
     }
+        
+                                // des tests pour quelques fonctions dans restaurateur.c 
+        
+        {   
+
+
+
+        int err2   , err1 ;
+        char * ligne1 = chercherLigneIdRestaurateur( 2 , "dataBase/tableRestaurants.csv" , &err2);        
+        TEST(strcmp(ligne1,  "2,Le Veg,13005,04 10 11 12 13,Vegetarien,2;3;4,24\n" )==0);// voir si on a bien recupérer la ligne qu'on veut 
+        char * ligne2 = chercherLigneIdRestaurateur( 1 , "dataBase/tableRestaurants.csv" , &err1);        
+        TEST(strcmp(ligne2,  "1,Chez Michel,13001,04 13 13 13 13,Provencal,1;4;5,50\n" )==0);// voir si on a bien recupérer la ligne qu'on veut 
+        TEST( err1 == 0 ); // voir si le fichier s'est ouvert correctement
+        TEST( err2 == 0); // voir si le fichier s'est ouvert correctement
+        
+        prestaurateur RestaurateurTest = (prestaurateur)malloc(sizeof(restaurateur));
+        RestaurateurTest = ajoutRestaurateurConstructeur( "3", "Joe's International House of Pancakes" , "13010" , "04 22 33 44 55" , "Americain" , "6;7" , "44" );
+  
+        TEST(0== strcmp(RestaurateurTest->id , "3") );
+        TEST( 0==strcmp(RestaurateurTest->nom , "Joe's International House of Pancakes"));
+        TEST( 0==strcmp(RestaurateurTest->codePostal , "13010") );
+        TEST( 0==strcmp(RestaurateurTest->telephone , "04 22 33 44 55" ));
+        TEST( 0==strcmp(RestaurateurTest->type , "Americain" ));
+        TEST( 0==strcmp(RestaurateurTest->menu , "6;7" ));
+        TEST( 0==strcmp(RestaurateurTest->solde , "44" ));
+
+        err1=3;// changer la valeur pour les  prochaines tests
+
+        ajoutRestaurateur(RestaurateurTest , &err1 , "fichierQuiNexistePas.csv");
+        TEST( err1 == 1 );    // voir si le fichier s'est ouvert correctement     
+        err1=3;// changer la valeur pour les  prochaines tests
+        ajoutRestaurateur(RestaurateurTest , &err1 , "dataBase/testDataBase/emptyFileRestaurateur.csv");
+        TEST_FILE( "dataBase/testDataBase/emptyFileRestaurateur.csv" , "dataBase/testDataBase/testEmptyFileRestaurateur.csv"  );// voir si on a bien écrit la chose qu'on a voulu 
+        TEST(err1 ==0 ) ; // voir si le fichier s'est ouvert correctement
+
+
+
+
+
+    }
 
     return tests_executed - tests_successful;
 }
