e83fa6a7c8e8b4113980af530808c81d64e73130
Aiman <aiman.mohassine@etu.univ-amu.fr>
2021-08-13 00:19:44 +0200

    Implementation de la fonction getfield

---
+++ lib/fonctionsCommunes.c
@@ -0,0 +1,14 @@
+#include "fonctionsCommunes.h"
+
+const char* getfield(char* line, int num)
+{
+    const char* tok;
+    for (tok = strtok(line, ",");
+            tok && *tok;
+            tok = strtok(NULL, ","))
+    {
+        if (!--num)
+            return tok;
+    }
+    return NULL;
+}
---
+++ lib/fonctionsCommunes.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+const char* getfield(char* line, int num); // Fonction permet la manipulation des fichier CSV (extraction de cellules dans un fichier CSV)
