827c8cd6110956853f822aef6b26f78a3448b5cc
Aiman <aiman.mohassine@etu.univ-amu.fr>
2021-08-12 22:56:18 +0200

    creation de la premiere partie du menu choix pour la connexion et la creation d un compte

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
+    char choix_1 ;
+    printf("Bienvenue dans le menu principale de LuminyEats Application\n") ;
+    do
+    {
+        printf("[1].Pour vous connecté\n[2].Pour créer un compte\n[q].Pour quitter\n");
+        scanf("%c", &choix_1) ;
+        // viderBuffer() ;
+        switch (choix_1)
+        {
+
+        /***********************/
+        /* Pour vous connecter */
+        /***********************/
+        case '1':
+            break;
+
+        /************************/
+        /* Pour créer un compte */
+        /************************/
+        case '2':
+            break;
+        }
+    }while (choix_1 != 'q');
     return 0;
 }
