80ac00da6c944f871855883baa1aed595cecc509
Aiman <aiman.mohassine@etu.univ-amu.fr>
2021-08-16 21:54:35 +0200

    Implementation de la fonction de la modification du solde client

--- lib/client.c
+++ lib/client.c
@@ -86,7 +86,7 @@ int afficherSoldeClient(int id){
     FILE *fichier;
     int solde ;
     char line[1024] ;
-    fichier = fopen("./db/client.csv", "r") ;
+    fichier = fopen("dataBase/tableClient.csv", "r") ;
     if(fichier != NULL){
         while (fgets(line, 1024, fichier))
         {
@@ -109,3 +109,59 @@ int afficherSoldeClient(int id){
     }
 }
 
+int modifierSoldeClient(int id, int modification){
+    FILE *fichier;
+    int solde ;
+    char line[1024] ;
+    int indicateur = 0 ;
+    fichier = fopen("dataBase/tableClient.csv", "r") ;
+    if(fichier != NULL){
+        while (fgets(line, 1024, fichier) && indicateur == 0)
+        {
+            // On parcours l'ensemble des id de ligne
+            char* tmp_1 = strdup(line);
+            // Si on trouve l'id rechercher
+            // On crée le tableau avec le meme id (en int) et le type de l'utilisateur
+            // 1. Pour restaurateur
+            if(atoi(getfield_2(tmp_1, 1)) == id){
+                solde = atoi(getfield_2(line, 5)) ;
+                solde = solde + modification ;
+                indicateur = 1 ;
+                fclose(fichier) ;
+                FILE* fichier_temp ;
+                fichier_temp = fopen("dataBase/client_temp.csv", "w") ;
+                fichier = fopen("dataBase/tableClient.csv", "r") ;
+                char ligneCopie[1024] ;
+                while(fgets(ligneCopie, 1024, fichier)){
+                    // free(tmp_1) ;
+                    tmp_1 = strdup(ligneCopie) ;
+                    char* tmp_2 = strdup(ligneCopie) ;
+                    char* tmp_3 = strdup(ligneCopie) ;
+                    char* tmp_4 = strdup(ligneCopie) ;
+                    char* tmp_5 = strdup(ligneCopie) ;
+                    char* id_client = getfield_2(tmp_2, 1) ;
+                    char* nom_client = getfield_2(tmp_3, 2) ;
+                    char* code_postal_client = getfield_2(tmp_4, 3) ;
+                    char* telephone_client = getfield_2(tmp_5, 4) ;
+                    // char* solde_client = getfield_2(tmp_6, 5) ;
+                    if(atoi(id_client) != id){
+                        fputs(ligneCopie, fichier_temp) ;
+                    }
+                    else{
+                        fprintf(fichier_temp, "%s,%s,%s,%s,%d\n", id_client, nom_client, code_postal_client, telephone_client, solde) ;
+                    }
+                }
+                fclose(fichier) ;
+                fclose(fichier_temp) ;
+                remove("dataBase/tableClient.csv");
+                //rename the file replica.c to original name
+                rename("dataBase/client_temp.csv", "dataBase/tableClient.csv");
+                return 1 ;
+            }
+        }
+        return 0;
+    }
+    else{
+        return -1 ;
+    }
+}
